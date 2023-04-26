deb7d66517575b2118dd6a137088532637b96fc6
Cody McKeon <codymckeon@gmail.com>
2023-04-26 16:45:43 -0700

    Add project to Github

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Hava Weeds Dealer</a>
+      </header>
+      <main>
+        Welcome Please fill out the form so that we can quote you. Form Fill.
+      </main>
+    </div>
+  );
+}
+
+export default App;
