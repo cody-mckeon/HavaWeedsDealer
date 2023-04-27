1522811888488afeec2e7c40f64c07db2716041c
Cody McKeon <codymckeon@gmail.com>
2023-04-26 18:41:16 -0700

    listed the services

---
+++ frontend/src/data.js
@@ -0,0 +1,20 @@
+const data = {
+  services: [
+    {
+      name: 'Spray Weeds',
+      slug: 'Weed Spray',
+      image: '/images/spray.jpg',
+      description:
+        'We charge based off of square footage and difficulty of spray',
+    },
+    {
+      name: 'Clean Up',
+      slug: 'Clean Weeds',
+      image: '/images/clean.jpg',
+      description:
+        'We charge based off of square footage and difficulty of spray',
+    },
+  ],
+};
+
+export default data;
